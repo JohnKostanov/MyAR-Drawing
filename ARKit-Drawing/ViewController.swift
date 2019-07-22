78e32ee55dfee245d0cbfcea9969500d7ae38174
JohnKostanov <kostanovd@icloud.com>
2019-07-22 09:40:10 +0200

    Add image

--- ARKit-Drawing/ViewController.swift
+++ ARKit-Drawing/ViewController.swift
@@ -2,7 +2,7 @@ import UIKit
 import SceneKit
 import ARKit
 
-class ViewController: UIViewController, ARSCNViewDelegate {
+class ViewController: UIViewController {
 
     // MARK: - Outlets
     @IBOutlet var sceneView: ARSCNView!
@@ -15,12 +15,28 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     // MARK: - Properties
     let configuration = ARWorldTrackingConfiguration()
     var selectedNode: SCNNode?
-    var objectMode: ObjectPlacementMode = .freeform
+    var objectMode: ObjectPlacementMode = .freeform {
+        didSet {
+            reloadConfiguration()
+        }
+    }
+    
+    var placedNodes = [SCNNode]()
+    var planeNodes = [SCNNode]()
+    
     var rootNode: SCNNode {
         return sceneView.scene.rootNode
     }
+    // MARK:- Custom Methods
+    func reloadConfiguration() {
+        configuration.detectionImages = (objectMode == .image) ? ARReferenceImage.referenceImages(inGroupNamed: "AR Resourses", bundle: nil) : nil
+        
+        configuration.planeDetection = [.horizontal]
+        
+        sceneView.session.run(configuration)
+    }
     
-    // MARK: - View Controllers Methods
+    // MARK: - UIViewController Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -30,7 +46,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        sceneView.session.run(configuration)
+        reloadConfiguration()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -39,6 +55,12 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     }
     
     // MARK: - Object Placemode Methods
+    func addNode(_ node:SCNNode, to parentNode: SCNNode) {
+        let cloneNode = node.clone()
+        parentNode.addChildNode(cloneNode)
+        placedNodes.append(cloneNode)
+    }
+    
     func addNodeInFront(_ node: SCNNode) {
         guard let currentFrame = sceneView.session.currentFrame else { return }
         
@@ -47,8 +69,15 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         translation.columns.3.z = -0.2
         node.simdTransform = matrix_multiply(transform, translation)
         
-        let cloneNode = node.clone()
-        rootNode.addChildNode(cloneNode)
+        addNode(node, to: rootNode)
+    }
+    
+    func nodeAdded(_ node: SCNNode, for anchor: ARPlaneAnchor) {
+        
+    }
+    func nodeAdded(_ node: SCNNode, for anchor: ARImageAnchor) {
+        guard let selectedNode = selectedNode else { return }
+        addNode(selectedNode, to: node)
     }
     
     // MARK: - Touches
@@ -56,7 +85,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         super.touchesBegan(touches, with: event)
         
         guard let node = selectedNode else { return }
-        guard let touch = touches.first else { return }
+        guard touches.first != nil else { return }
         
         switch objectMode {
         case .freeform:
@@ -109,5 +138,16 @@ extension ViewController: OptionsViewControllerDelegate {
     
     func resetScene() {
         dismiss(animated: true, completion: nil)
+    }
+}
+
+// MARK: - ARSCNViewDelegate
+extension ViewController: ARSCNViewDelegate {
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        if let imageAnchor = anchor as? ARImageAnchor {
+            nodeAdded(node, for: imageAnchor)
+        } else if let planeAnchor = anchor as? ARPlaneAnchor {
+            nodeAdded(node, for: planeAnchor)
+        }
     }
 }
